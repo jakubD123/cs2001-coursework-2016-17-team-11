0895a5bb03684603eab113cfa98558f07db18c57
zahirajamil <1540418@my.brunel.ac.uk>
2017-03-08 17:04:51 +0000

    Add files via upload

---
+++ interests1.java
@@ -0,0 +1,13 @@
+package com.example.zohaib.unibuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class interests1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interests1);
+    }
+}
